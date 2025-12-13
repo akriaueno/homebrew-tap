3761bfe87ba4add40217cb28e0c9c58182d36c98
akriaueno <26422336+akriaueno@users.noreply.github.com>
2025-12-13 13:24:10 +0000

    why v0.1.4

--- Formula/why.rb
+++ Formula/why.rb
@@ -1,8 +1,8 @@
 class Why < Formula
   desc "Show where a command on your system really comes from"
   homepage "https://github.com/akriaueno/why-cli"
-  url "https://github.com/akriaueno/why-cli/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "dd6d3bb0aa6f73369884916f66a519b221ae5ff6b9bb64a8acf9303432d92c1a"
+  url "https://github.com/akriaueno/why-cli/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "a2f590ff9e30e55d48c0220cbb6023c89ea1b00738e25264383ceed79180d2dd"
   license "MIT"
 
   depends_on "nim" => :build
