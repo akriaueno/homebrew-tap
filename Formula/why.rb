242d21d91be4e26c40bd36882072c98ac4dcd337
akriaueno <26422336+akriaueno@users.noreply.github.com>
2025-12-15 14:24:15 +0000

    why v0.1.10
    Closes #10.
    
    Signed-off-by: akiraueno <akiraueno@outlook.com>

--- Formula/why.rb
+++ Formula/why.rb
@@ -1,15 +1,9 @@
 class Why < Formula
   desc "Show where a command on your system really comes from"
   homepage "https://github.com/akriaueno/why-cli"
-  url "https://github.com/akriaueno/why-cli/archive/refs/tags/v0.1.9.tar.gz"
-  sha256 "2ac19122094b2aec5541d59dfd6e7bc48bc688d5bd301169b394a1869e85cd37"
+  url "https://github.com/akriaueno/why-cli/archive/refs/tags/v0.1.10.tar.gz"
+  sha256 "f71536e76e043e525d6a6f58c5c541f1c6bc4bb93b09c00f88a90e7b4c909c50"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/akriaueno/homebrew-tap/releases/download/why-0.1.9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cec1093bc750bff5ab2a2056dc2cd13dda372c6dfa157565772d258bd04ba11b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "08e1ac53f408fbf10dc545154909892dc0752e1e98a39ed8ed6bcb64102cf28b"
-  end
 
   depends_on "nim" => :build
 
